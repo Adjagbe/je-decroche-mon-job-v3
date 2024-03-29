d795b338d820e73bcbcf0c52e83c0b606e5e36d5
Adjagbe josephine <143086238+JAdjagbe@users.noreply.github.com>
2024-03-29 18:44:24 +0000

    push2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+import './index.css';
+import React from 'react'
+import Home from './PAGES/Home'
+import Login from './PAGES/Login'
+import Register from './PAGES/Register'
+import Postulant from './PAGES/Postulant'
+import PassForgot from './PAGES/PassForgot'
+import Notification_page from './PAGES/Notification_page'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from "./SCREENS/FOOTER";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+       
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="pass_forgot" element={<PassForgot />} />
+          <Route path="register" element={<Register />} />
+          <Route path="notification_page" element={<Notification_page />} />
+          <Route path="dashboard" element={<Postulant />} />
+
+      </Routes>
+      <Footer/>
+    </Router>
+    
+  )
+}
