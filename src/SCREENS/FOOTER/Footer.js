857cb40f57ba9e552e520fdf98aff121e9a36cb0
Adjagbe josephine <104694773+Adjagbe@users.noreply.github.com>
2024-03-30 20:51:10 +0000

    Update Footer.js

--- src/SCREENS/FOOTER/Footer.js
+++ src/SCREENS/FOOTER/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-      <div className='bg-white text-gray-600 h-24 text-center pt-10'>Designed by Boost Business | Teamdev</div>
+      <div className='bg-white text-gray-600 h-24 text-center pt-10'>Designed by Boost Business | Teamdev voir laala</div>
   )
 }
 
